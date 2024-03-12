3948257a5d1cb7391c17db150e3953eda0363a0f
Wai Lwin Min Thant <wailwinminthant7@gmail.com>
2024-03-12 21:39:52 +0630

    progressbar

---
+++ app.js
@@ -0,0 +1,33 @@
+const getdownloadbtn = document.querySelector('.download-btn');
+const getprogressbar = document.querySelector('.progress-bar');
+const seturl = "https://linkedin.com";
+
+getdownloadbtn.addEventListener('click',function(){
+	// console.log("hay i am working");
+
+	var setwidth = 0;
+
+	let setinv = setInterval(progressinc,100);
+
+	
+	function progressinc(){
+
+		if(setwidth >= 100){
+			setwidth = 0;
+			clearInterval(setinv);
+
+			// redirext
+			window.location.href = seturl;
+		}else{
+			setwidth++;
+			getprogressbar.style.width = `${setwidth}%`;
+			getprogressbar.setAttribute('data-inc',`${setwidth}%`);
+			getdownloadbtn.setAttribute('disabled',true);
+		}
+
+		// console.log(setwidth);
+
+	}
+
+
+});
